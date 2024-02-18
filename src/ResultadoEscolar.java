3d430b3fda2b083d986c181c7aaa5e96b7855458
Lucas Oliveira <lucaoxt@gmail.com>
2024-02-18 10:51:50 -0300

    Condição ternária

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -2,11 +2,15 @@ public class ResultadoEscolar {
 
     public static void main(String[] args) {
         Integer nota = 8;
-        if (nota >= 7)
-            System.out.println("Aprovado");
-        else if (nota >= 5 && nota < 7)
-            System.out.println("Prova Recuperação");
-        else
-            System.out.println("Reprovado");
+        // if (nota >= 7)
+        //     System.out.println("Aprovado");
+        // else if (nota >= 5 && nota < 7)
+        //     System.out.println("Prova Recuperação");
+        // else
+        //     System.out.println("Reprovado");
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+            
+        System.out.println(resultado);
     }
 }
