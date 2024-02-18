c63f45380a0729466b6043896931513cda6788e4
Lucas Oliveira <lucaoxt@gmail.com>
2024-02-18 10:46:48 -0300

    Condicional encadeada

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -4,6 +4,8 @@ public class ResultadoEscolar {
         Integer nota = 8;
         if (nota >= 7)
             System.out.println("Aprovado");
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova Recuperação");
         else
             System.out.println("Reprovado");
     }
