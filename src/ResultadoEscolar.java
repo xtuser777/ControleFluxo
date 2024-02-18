b8066e3bbceffd85e9def85daff977b705a01fd8
Lucas Oliveira <lucaoxt@gmail.com>
2024-02-18 10:41:57 -0300

    Condicional composta

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,10 @@
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+        Integer nota = 8;
+        if (nota >= 7)
+            System.out.println("Aprovado");
+        else
+            System.out.println("Reprovado");
+    }
+}
